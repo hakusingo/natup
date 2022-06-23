83f811c1419fcb25873f6a14fb4328f61d9a53a7
hakusingo <hakusingo@gmail.com>
2022-06-23 11:07:04 +0900

    secondSection

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,23 +1,10 @@
 import React from 'react';
-import { useState, useEffect } from "react";
 
 const About = () => {
-  const [count, setCount] = useState(0)
-  useEffect(() => {
-    get()
-  }, [count])
-
-  function get() {
-    let h2 = document.querySelector("h2").innerText
-    console.log(h2)
-  }
 
   return (
     <>
-      <h2>UseEffect</h2>
-      <button onClick={() => {
-        setCount((prevCount) => prevCount + 1)
-      }}>{count}</button>
+  
     </>
   )
 }
